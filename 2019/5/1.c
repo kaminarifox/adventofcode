410f13c50caf503f7f3bc09c32b8adb7a857d466
Eduard Il'in <ei2@ukrosoft.com.ua>
2019-12-11 19:49:09 +0200

    5, 10 days

--- 2019/5/1.c
+++ 2019/5/1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int* loadCode() {
+void loadCode(int* buffer, int* len) {
     FILE *fptr = fopen("./input", "r");
     if (fptr == NULL) {
         printf("Input file not found!\n");
@@ -10,15 +10,17 @@ int* loadCode() {
     }
 
     char opcode[10] = "";
-    int* buffer = malloc(sizeof(int));
-    int len = 1;
+    buffer = malloc(sizeof(int));
+    *len = 1;
 
     while (1) {
         char c = fgetc(fptr);
         if (c == ',' || c == EOF) {
-            buffer[len - 1] = atoi(opcode);
-            buffer = realloc(buffer, (len + 1) * sizeof(int));
-            len++;
+            buffer[*len - 1] = atoi(opcode);
+            printf("%d ",  buffer[*len - 1]);
+            // FIXME: Remove realloc on EOF
+            buffer = realloc(buffer, (*len + 1) * sizeof(int));
+            *len += 1;
             opcode[0] = '\0';
         } else {
             strncat(opcode, &c, 1);
@@ -29,10 +31,10 @@ int* loadCode() {
             break;
         }
     }
-
-    return buffer;
 }
 
 void main() {
-    int* code = loadCode();
+    int* buffer;
+    int len;
+    loadCode(buffer, &len);
 }
