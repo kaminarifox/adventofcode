bf211800c045eb530db948b92fb3e39ecf9deb98
Eduard Il'in <ei2@ukrosoft.com.ua>
2019-12-13 21:59:49 +0200

    5 day

--- 2019/5/1.c
+++ 2019/5/1.c
@@ -2,38 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-void loadCode(int** buffer, int* len) {
+int loadCode(int **buffer, FILE *fptr) {
+    int size = 0, *tmpBuf = calloc(0, sizeof(int));
+    char c, opcode[10] = "";
+
+    do {
+        c = fgetc(fptr);
+        if (c != ',' && c != EOF) {
+            strncat(opcode, &c, 1);
+        } else {
+            size += 1;
+            int *extBuf = realloc(tmpBuf, size * sizeof(int));
+            if (extBuf == NULL) {
+                printf("Buffer allocation error!");
+                exit(1);
+            }
+            extBuf[size - 1] = atoi(opcode);
+            opcode[0] = '\0';
+            tmpBuf = extBuf;
+        }
+
+    } while (c != EOF);
+    *buffer = tmpBuf;
+
+    return size;
+}
+
+void main() {
     FILE *fptr = fopen("./input", "r");
     if (fptr == NULL) {
         printf("Input file not found!\n");
         exit(1);
     }
 
-    char opcode[10] = "";
-    *buffer = malloc(0);
-    *len = 0;
+    int *buffer;
+    int size = loadCode(&buffer, fptr);
 
-    char c;
-    do {
-        c = fgetc(fptr);
-        if (c != ',' && c != EOF) {
-            strncat(opcode, &c, 1);
-        } else {
-            printf("%d", *len);
-            *buffer[*len] = 1; //atoi(opcode);
-            *len += 1;
-            *buffer = (int*) realloc(*buffer, 1000 * sizeof(int));
-            opcode[0] = '\0';      
+
+    int ptr = 0;
+    int opcode = buffer[ptr];
+
+    while (opcode != 99) {
+        switch (opcode) {
+            case 1:
+                buffer[ptr + 3] = buffer[ptr + 1] + buffer[ptr + 2];
+                ptr += 4;
+                break;
+            case 2:
+                buffer[ptr + 3] = buffer[ptr + 1] * buffer[ptr + 2];
+                ptr += 4;
+                break;
+            case 3:
+                int input;
+                scanf("$d", &input);
+                buffer[ptr + 1] = input;
+                ptr += 2;
+                break;
+            case 4:
+                printf("$d\n", buffer[ptr + 1]);
+                ptr += 2;
+                break;
+            default:
+                printf("Invalid opcode [%d]", opcode);
+                break;
         }
-      
-    } while (c != EOF);
-    
+        opcode = buffer[ptr];
+    }
+
     fclose(fptr);
-
-}
-
-void main() {
-    int* buffer;
-    int len;
-    loadCode(&buffer, &len);
+    free(buffer);
 }
