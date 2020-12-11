e3963a7eaa72d48f54033f2a35e5f08d6c5addb2
Eduard Ilin <ei2@ukrosoft.com.ua>
2020-12-11 19:43:54 +0200

    Day 8

--- year-2020/src/8/main.rs
+++ year-2020/src/8/main.rs
@@ -2,54 +2,60 @@ use std::process::exit;
 
 static INPUT: &str = include_str!("input");
 
-struct Opcode<'a> {
-    cmd: &'a str,
+struct Opcode {
+    cmd: String,
     operand: i32,
     exec_number: usize,
 }
 
-struct Cpu<'b> {
-    rom: Vec<Opcode<'b>>,
+struct Cpu {
+    rom: Vec<Opcode>,
     acc: i32,
-    pc: usize,
+    pc: i32,
 }
 
-impl Cpu<'_> {
-    pub fn init(input: &'static str) -> Self {
+impl Cpu {
+    pub fn init(input: &str) -> Self {
         let mut rom = vec![];
         let opcodes = input.split('\n').collect::<Vec<&str>>();
         for opcode in opcodes {
             let command = opcode.split(' ').collect::<Vec<&str>>();
             rom.push(Opcode {
-                cmd: command[0],
-                operand: (command[1])[1..].parse::<i32>().unwrap(),
+                cmd: command[0].to_string(),
+                operand: command[1].parse::<i32>().unwrap(),
                 exec_number: 0,
-            })
+            });
         }
 
-        return Cpu { rom, acc: 0, pc: 0 };
+        return Self { rom, acc: 0, pc: 0 };
     }
 
-    pub fn next(&mut self, next_pc: usize) {
-        let mut opcode = self.rom.get_mut(self.pc).unwrap();
+    pub fn next(&mut self, next_pc: i32) {
+        let mut opcode = self.rom.get_mut(self.pc as usize).unwrap();
         opcode.exec_number += 1;
         self.pc += next_pc;
-        if self.pc > (self.rom.len() - 1) {
-            self.pc -= self.rom.len() - 1;
-        }
     }
 
     pub fn run(&mut self) {
         loop {
-            let opcode = self.rom.get(self.pc).unwrap();
-            match opcode.cmd {
+            if self.rom[self.pc as usize].exec_number == 1 {
+                println!("FAIL: {}", self.acc);
+                break;
+            }
+
+            let operand = self.rom[self.pc as usize].operand;
+
+            match self.rom[self.pc as usize].cmd.as_str() {
                 "nop" => self.next(1),
                 "acc" => {
-                    self.acc += opcode.operand;
-                    println!("{}", self.acc);
+                    self.acc += operand;
                     self.next(1);
                 }
-                "jmp" => self.next(opcode.operand as usize),
+                "jmp" => self.next(operand),
+                "end" => {
+                    println!("EXIT: {}", self.acc);
+                    exit(0)
+                }
                 _ => (),
             }
         }
@@ -57,6 +63,19 @@ impl Cpu<'_> {
 }
 
 fn main() {
+    // let mut buffer = String::new();
+    // io::stdin().lock().read_to_string(&mut buffer).unwrap();
+
+    // First puzzle
     let mut cpu = Cpu::init(INPUT);
     cpu.run();
+    println!("\n");
+
+    // Second puzzle
+    for m in INPUT.match_indices("jmp") {
+        let mut input = String::from(INPUT);
+        input.replace_range(m.0..m.0 + 3, "nop");
+        let mut cpu = Cpu::init(input.as_str());
+        cpu.run();
+    }
 }
