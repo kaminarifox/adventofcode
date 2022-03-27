1817257f9402bf37fb6ebc2df875f99e58c62b44
Eduard Ilin <ei2@ukrosoft.com.ua>
2022-03-27 18:15:27 +0300

    y2021d08 solution

--- year-2021/examples/8/main.rs
+++ year-2021/examples/8/main.rs
@@ -1,72 +1,98 @@
-// https://adventofcode.com/2021/day/8
 use std::fs;
 
-struct SegmentDigit {
-    num: i32,
-    seg_count: i32,
-    variations: Vec<String>,
+#[derive(Debug)]
+struct Display {
+    digits: Vec<String>
 }
 
-impl SegmentDigit {
-    fn make_variations(len: i32, parts: Vec<char>) -> Vec<String> {
-        let mut out: Vec<String> = vec![];
+impl Display {
+    fn new() -> Self {
+        Display { digits: vec![String::new(); 10] }
+    }
 
+    fn resolve_wires(&mut self, sample: &Vec<&str>) {
+        self.digits[1] = sample.into_iter().find(|x| x.len() == 2).unwrap().to_string();
+        self.digits[4] = sample.into_iter().find(|x| x.len() == 4).unwrap().to_string();
+        self.digits[7] = sample.into_iter().find(|x| x.len() == 3).unwrap().to_string();
+        self.digits[8] = sample.into_iter().find(|x| x.len() == 7).unwrap().to_string();
 
-        for l in 0..len {
-            for i in parts.len() {
-                let mut part = String::new(parts[i]);
+        // Find three
+        self.digits[3] = sample.into_iter().filter(|x| x.len() == 5)
+            .find(|x| self.diff(self.digits[7].clone(), x.to_string()).len() == 2).unwrap().to_string();
 
-                let parts_next = parts.clone();
-                parts_next.remove(i);
+        // Find six
+        self.digits[6] = sample.into_iter().filter(|x| x.len() == 6)
+            .find(|x| {
+                !x.contains(self.digits[1].chars().nth(0).unwrap()) || !x.contains(self.digits[1].chars().nth(1).unwrap())
+            }).unwrap().to_string();
 
-                for j in parts_next.len() {
-
-
-                }
+        // Find two and five
+        sample.into_iter().filter(|x| x.len() == 5).for_each(|x| {
+            if self.diff(self.digits[6].clone(), x.to_string()).len() == 1 {
+                self.digits[5] = x.to_string();
+            } else if self.digits[3].ne(x) {
+                self.digits[2] = x.to_string();
             }
-        }
+        });
 
-
-
-        for i in 0..len {
-
-            let part =
-
-            for j in 0..len {
-                for k in 0..parts.len() {
-                    let mut part =
-                }
+        // Find zero and nine
+        sample.into_iter().filter(|x| x.len() == 6).for_each(|x| {
+            let diff = self.diff(self.digits[3].clone(), x.to_string());
+            if diff.len() == 1 {
+                self.digits[9] = x.to_string();
+            } else if self.digits[6].ne(x) {
+                self.digits[0] = x.to_string();
             }
-        }
+        });
+    }
 
-        for char in parts {
-            for i in 0..len {
-                let mut next_parts = parts.clone();
-                next_parts.remove(next_parts.iter().position(|x| *x == char));
+    fn diff(&self, src: String, dest: String) -> Vec<char> {
+        let mut out: Vec<char> = vec![];
 
-                if next_parts
+        for char in ['a', 'b', 'c', 'd', 'e', 'f', 'g'] {
+            if src.contains(char) ^ dest.contains(char) {
+                out.push(char);
             }
         }
 
         return out;
     }
 
-    pub fn new(num: i32) -> Self {
-        let parts = match num {
-            1 => 2,
-            7 => 3,
-            4 => 4,
-            2 | 3 | 5 => 5,
-            6 |9 => 6,
-            8 => 7,
-        };
-
-        SegmentDigit { num, parts, variations: Self::make_variations(parts) }
+    fn decode(&self, number: &str) -> i32 {
+        self.digits.iter()
+            .position(|x| self.diff(x.clone(), number.to_string()).len() == 0).unwrap() as i32
     }
 }
 
-
-
 fn main() {
+    let input = fs::read_to_string("./input.txt").unwrap();
+    let mut firs_result = [0, 0, 0, 0, 0];
+    let mut second_result = 0;
 
+    for line in input.lines() {
+        let input_split = line.split(" | ");
+        let digits_samples: Vec<&str> = input_split.clone().nth(0).unwrap().split(' ').collect();
+        let encoded_numbers: Vec<&str> = input_split.clone().nth(1).unwrap().split(' ').collect();
+
+        let mut display = Display::new();
+        display.resolve_wires(&digits_samples);
+
+        let mut decoded_number= String::new();
+        for encoded_number in encoded_numbers {
+            let decoded_digit = display.decode(encoded_number);
+            decoded_number.push_str(decoded_digit.to_string().as_str());
+            match decoded_digit {
+                1 => firs_result[0] += 1,
+                4 => firs_result[1] += 1,
+                7 => firs_result[2] += 1,
+                8 => firs_result[3] += 1,
+                _ => { }
+            }
+        }
+        second_result += decoded_number.parse::<i32>().unwrap();
+    }
+
+    println!("{:?}", firs_result.iter().sum::<i32>());
+    println!("{}", second_result);
 }
+
