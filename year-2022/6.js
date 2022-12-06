95c317ca923ef94e799fbd1e81cab714b9de791f
Eduard Ilin <ei2@ukrosoft.com.ua>
2022-12-06 14:14:32 +0200

    year 2022, day 6, fix loop condition

--- year-2022/6.js
+++ year-2022/6.js
@@ -1,7 +1,7 @@
 const input = `bvwbjplbgvbhsrlpgdmjqwftvncz`;
 
 const seqSize = 14;
-for (let i = 0; i < input.length - seqSize; i++) {
+for (let i = 0; i <= input.length - seqSize; i++) {
     const buf = {};
     for (let j = 0; j < seqSize; j++)
         buf[input[i + j]] = true
